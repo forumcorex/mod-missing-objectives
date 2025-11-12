a25fde0500a5a65f3bcffce67dbf9745b153b06d
Trimitor <73941033+Trimitor@users.noreply.github.com>
2025-11-12 22:49:01 +0300

    Chore. Quests in Ragefire Chasm https://github.com/forumcorex/mod-missing-objectives/issues/4
    Added missing POIs for quests in Ragefire Chasm:
     - [x] https://www.wowhead.com/wotlk/quest=5728/hidden-enemies
     - [x] https://www.wowhead.com/wotlk/quest=14356/the-power-to-destroy
     - [x] https://www.wowhead.com/wotlk/quest=5723/testing-an-enemys-strength
     - [x] https://www.wowhead.com/wotlk/quest=5761/slaying-the-beast
     - [ ] ~~https://www.wowhead.com/wotlk/quest=5724/returning-the-lost-satchel~~ No need to add POI
     - [ ] ~~https://www.wowhead.com/wotlk/quest=5722/searching-for-the-lost-satchel~~ No need to add POI
    
    **Gallery**
    https://imgur.com/a/RGG8OfS

---
+++ data/sql/db-world/base/mo_2025_11_12_00.sql
@@ -0,0 +1,47 @@
+DELETE FROM `quest_poi` WHERE `QuestID`=5728 AND `id`=1 AND `ObjectiveIndex`>=0;
+INSERT INTO `quest_poi` (`QuestID`, `id`, `ObjectiveIndex`, `MapID`, `WorldMapAreaId`, `Floor`, `Priority`, `Flags`, `VerifiedBuild`) VALUES 
+(5728, 1, 0, 389, 680, 1, 0, 3, 0);
+
+DELETE FROM `quest_poi` WHERE `QuestID`=5728 AND `id`=2 AND `ObjectiveIndex`>=0;
+INSERT INTO `quest_poi` (`QuestID`, `id`, `ObjectiveIndex`, `MapID`, `WorldMapAreaId`, `Floor`, `Priority`, `Flags`, `VerifiedBuild`) VALUES 
+(5728, 2, 1, 389, 680, 1, 0, 3, 0);
+
+DELETE FROM `quest_poi_points` WHERE `QuestID`=5728 AND `Idx1`=1;
+INSERT INTO `quest_poi_points` (`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES 
+(5728, 1, 0, -386, 143, 0);
+
+DELETE FROM `quest_poi_points` WHERE `QuestID`=5728 AND `Idx1`=2;
+INSERT INTO `quest_poi_points` (`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES 
+(5728, 2, 0, -362, 200, 0);
+
+DELETE FROM `quest_poi` WHERE `QuestID`=5761 AND `id`=1 AND `ObjectiveIndex`=4;
+INSERT INTO `quest_poi` (`QuestID`, `id`, `ObjectiveIndex`, `MapID`, `WorldMapAreaId`, `Floor`, `Priority`, `Flags`, `VerifiedBuild`) VALUES
+(5761, 1, 4, 389, 680, 1, 0, 1, 0);
+
+DELETE FROM `quest_poi_points` WHERE `QuestID`=5761 AND `Idx1`=1;
+INSERT INTO `quest_poi_points` (`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES
+(5761, 1, 0, -244, 151, 0);
+
+DELETE FROM `quest_poi` WHERE `QuestID`=14356 AND `id`=1 AND `ObjectiveIndex`=4;
+INSERT INTO `quest_poi` (`QuestID`, `id`, `ObjectiveIndex`, `MapID`, `WorldMapAreaId`, `Floor`, `Priority`, `Flags`, `VerifiedBuild`) VALUES
+(14356, 1, 4, 389, 680, 1, 0, 1, 0);
+
+DELETE FROM `quest_poi_points` WHERE `QuestID`=14356 AND `Idx1`=1;
+INSERT INTO `quest_poi_points` (`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES
+(14356, 1, 0, -209, 49, 0),
+(14356, 1, 1, -166, 199, 0),
+(14356, 1, 2, -365, 286, 0),
+(14356, 1, 3, -421, 189, 0),
+(14356, 1, 4, -400, 121, 0),
+(14356, 1, 5, -341, 119, 0);
+
+DELETE FROM `quest_poi` WHERE `QuestID`=5723 AND `id`>=3 AND `ObjectiveIndex`>=0;
+INSERT INTO `quest_poi` (`QuestID`, `id`, `ObjectiveIndex`, `MapID`, `WorldMapAreaId`, `Floor`, `Priority`, `Flags`, `VerifiedBuild`) VALUES
+(5723, 3, 0, 389, 680, 1, 0, 3, 0);
+
+DELETE FROM `quest_poi_points` WHERE `QuestID`=5723 AND `Idx1`>=3;
+INSERT INTO `quest_poi_points` (`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES
+(5723, 3, 0, -148, 10, 0),
+(5723, 3, 1, -138, -67, 0),
+(5723, 3, 2, -281, -103, 0),
+(5723, 3, 3, -287, -13, 0);
